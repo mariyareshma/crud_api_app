5a0b0bfc35e33f26d7d428330b95d85af388cd14
Mariya Reshma <rreshma10653@gmail.com>
2023-02-22 11:40:21 +0530

    adding search bar in crud api app

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -9,11 +9,16 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
+  List<PersonDetails> filteredList = <PersonDetails>[];
+
+  List<PersonDetails> originalList = <PersonDetails>[];
+
+  var searchItem = '';
+
   Future<PersonDetails?> showPersonAddEditDialog(PersonDetails? person) async {
     return await showDialog(
       context: context,
@@ -51,6 +56,27 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  Future getDataFromApi() async {
+    originalList = await getPersonDetails();
+  }
+
+  Future<List<PersonDetails>> filterResults() async {
+    await getDataFromApi();
+
+    filteredList = originalList
+        .where((element) =>
+            element.name!.contains(searchItem) ||
+            element.age!.toString().contains(searchItem))
+        .toList();
+
+    return filteredList;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Crud App')),
@@ -80,15 +106,20 @@ class _HomePageState extends State<HomePage> {
           child: CircularProgressIndicator(),
         );
       },
-      future: getPersonDetails(),
+      future: filterResults(),
     );
   }
 
   Widget getPersonWidget(List<PersonDetails> data) {
-    if (data.isEmpty) {
-      return const Center(child: Text('there is no data'));
-    }
-    return ListView(children: getPersonWidgets(data));
+    // if (data.isEmpty) {
+    //   return const Center(child: Text('there is no data'));
+    // }
+    return Column(
+      children: [
+        searchBar(),
+        Expanded(child: ListView(children: getPersonWidgets(data))),
+      ],
+    );
   }
 
   List<Widget> getPersonWidgets(List<PersonDetails> persons) {
@@ -99,6 +130,7 @@ class _HomePageState extends State<HomePage> {
           var canDelete = await showDeleteOption();
           if (canDelete!) {
             await deletePerson(person);
+            await filterResults();
             setState(() {});
           }
         },
@@ -106,6 +138,7 @@ class _HomePageState extends State<HomePage> {
           var newPerson = await showPersonAddEditDialog(person);
           if (newPerson != null) {
             await editPerson(newPerson);
+            await filterResults();
             setState(() {});
           }
         },
@@ -117,4 +150,38 @@ class _HomePageState extends State<HomePage> {
 
     return widgets;
   }
+
+  Widget searchBar() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        keyboardType: TextInputType.name,
+        decoration: const InputDecoration(
+          constraints: BoxConstraints(minWidth: 20, minHeight: 20),
+          hintText: 'search',
+          labelText: 'search the crud in List',
+          icon: Icon(Icons.search),
+        ),
+        onChanged: (value) async {
+          searchItem = value;
+          await filterResults();
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  // void applyFilter() {
+  //   setState(() {
+  //   //   filteredList = data
+  //   //       .where((element) =>
+  //   //           element.name!.contains(searchItem) &&
+  //   //           element.age!.toString().contains(searchItem))
+  //   //       .toList();
+  //   // });
+  // }
 }
